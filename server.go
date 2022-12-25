d1afcb7f8b056af37fdbbf422167fd1b86a6209f
Rungtiwa Inha <rungtiwa.in@baac.or.th>
2022-12-25 21:24:04 +0700

    connect to database

--- server.go
+++ server.go
@@ -1,11 +1,64 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+
+	"github.com/labstack/echo"
+	_ "github.com/lib/pq"
 )
+
+type Err struct {
+	Message string `json:"message"`
+}
+
+type Expense struct {
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Amount float64  `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
+}
+
+var db *sql.DB
 
 func main() {
 	fmt.Println("Please use server.go for main file")
 	fmt.Println("start at port:", os.Getenv("PORT"))
+
+	var err error
+	db, err = sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	defer db.Close()
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS expenses (
+		id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[]
+	);
+	`
+	_, err = db.Exec(createTb)
+
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+	e := echo.New()
+
+	e.GET("/health", healthHandler)
+
+	log.Println("Server started at :2565")
+	log.Fatal(e.Start(":2565"))
+}
+
+func healthHandler(c echo.Context) error {
+	return c.JSON(http.StatusOK, "OK")
 }
