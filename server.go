53f04cf221e516f3659232f9ffb7aa37239cc52b
Rungtiwa Inha <rungtiwa.in@baac.or.th>
2022-12-28 20:44:36 +0700

    add middleware

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"github.com/rungtiwa-in/assessment/expense"
 )
 
@@ -12,6 +13,20 @@ func main() {
 	expense.InitDB()
 
 	e := echo.New()
+
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			auth := c.Request().Header.Get("Authorization")
+			if auth != "November 10, 2009" {
+				return c.JSON(http.StatusUnauthorized, "Unauthorized")
+			}
+
+			return next(c)
+		}
+	})
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	e.GET("/health", healthHandler)
 	e.POST("/expenses", expense.CreateExpenseHandler)
