11cc7bc2f0092d16bdb3aec89bf62caf8acc8941
Rungtiwa Inha <rungtiwa.in@baac.or.th>
2022-12-28 20:45:49 +0700

    remove unused func

--- server.go
+++ server.go
@@ -28,13 +28,8 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	e.GET("/health", healthHandler)
 	e.POST("/expenses", expense.CreateExpenseHandler)
 
 	log.Println("Server started at :2565")
 	log.Fatal(e.Start(":2565"))
-}
-
-func healthHandler(c echo.Context) error {
-	return c.JSON(http.StatusOK, "OK")
 }
